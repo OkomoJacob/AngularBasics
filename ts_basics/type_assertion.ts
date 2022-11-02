e80a6418a769560dfe7acf50291307e2233cfa33
OkomoJacob <okomo.code@gmail.com>
2022-11-02 05:53:29 +0300

    Type affirmation in TS :pencil2:

--- ts_basics/type_assertion.ts
+++ ts_basics/type_assertion.ts
@@ -1,4 +1,9 @@
-let mess = "type string";
-let printMess = mess.indexOf('g');
+let mess;
+mess = "type string";
+
+// Assert that mess is trsing, not any.
+let printMess = (<string>mess).endsWith('i');
+let alaternativePrint = (mess as string).endsWith('e');
 
 console.log(printMess);
+// console.log(alaternativePrint);

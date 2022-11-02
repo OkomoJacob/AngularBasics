e91d31dbd3ca81384a380b8ec20ddcec590a0b7c
OkomoJacob <okomo.code@gmail.com>
2022-11-02 16:58:26 +0300

    Interfcauings :pencil2:

---
+++ ts_basics/interfaces.ts
@@ -0,0 +1,13 @@
+// When working with a function which takes multiple arguments, you interfcae them(But using the Pascal naming convention)
+interface Point{
+    x: number;
+    y: number;
+}
+let drawPoint = (point: Point) =>{
+    // Function to draw point
+    
+}
+drawPoint({
+    x: 1,
+    y: 5,
+})

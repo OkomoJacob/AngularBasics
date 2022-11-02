f998a58c830d54e5753958496702277666fe05c6
OkomoJacob <okomo.code@gmail.com>
2022-11-02 16:57:50 +0300

    Arrow funcs :pencil2:

---
+++ ts_basics/arrow_functions.js
@@ -0,0 +1,9 @@
+// Traditional function expression
+const addNums = function(num1, num2){
+    return(num1 + num2);
+}
+
+console.log(addNums(12, 78));
+// Arrow functions
+(num1, num2) => addNums(12 + 32);
+console.log(addNums);

34ae3a6dd45dec2ee7230fffda6e8dbb369d6881
OkomoJacob <okomo.code@gmail.com>
2022-11-03 12:21:45 +0300

    OOP in TS, getting started :rocket: :pencil2:

--- ts_basics/classes.ts
+++ ts_basics/classes.ts
@@ -1,15 +1,19 @@
 // When working with a function which takes multiple arguments, you interface them(But using the Pascal naming convention)
 class NewPoint {
-    a: number | undefined;
-    b: number | undefined;
+    x: number | undefined;
+    y: number | undefined;
+
+
+    constructor(x?: number, y?:number){ // Initiazlie point to set the values of members of an object. 
+        this.x = x;
+        this.y = y;
+    }
     
-    draw(){
+    drawPoint(){
     // Function to draw point
-    console.log('X: ' + this.a + ' and Y: ' + this.b);
+    console.log('X: ' + this.x + ' and Y: ' + this.y);
     }
 }
 
-let point = new NewPoint; // the let point is an object( Instance of a class)
-point.a = 2;
-point.b = 5;
-point.draw();
+let newPoint = new NewPoint(5, 8); // the let point is an object( Instance of a class)
+newPoint.drawPoint();

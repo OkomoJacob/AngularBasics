0e71bfdbb01be7791dbc0ff7ff07d1f6947bd65e
OkomoJacob <okomo.code@gmail.com>
2022-11-03 05:57:01 +0300

    Interfacing result, to take in more inputs variables :pencil2:

--- ts_basics/interfaces.ts
+++ ts_basics/interfaces.ts
@@ -1,13 +1,12 @@
 // When working with a function which takes multiple arguments, you interfcae them(But using the Pascal naming convention)
 interface Point{
-    x: number;
-    y: number;
+    x: number,
+    y: number,
 }
 let drawPoint = (point: Point) =>{
     // Function to draw point
-    
 }
 drawPoint({
     x: 1,
-    y: 5,
+    y: 5
 })

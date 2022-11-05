35b60b6b9cb86feab1a8eb486b689bd768fd5d51
okomojacob <okomo.code@gmail.com>
2022-11-05 15:33:25 +0300

    Getting started :tada: with components & services, :zap: :fire:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { CoursesService } from './courses.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
+@NgModule({ //Decorator to convert plain TS into a Module
+  declarations: [ // Add all components that are part of this module.
+    AppComponent,
+    CoursesComponent,
+    // AuthComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ // Register all dependices that all components in this module are dependent on here
+    CoursesService
+  ], 
   bootstrap: [AppComponent]
 })
 export class AppModule { }

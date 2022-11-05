3bbf89fa6a364846c775238f29e4aeead87770fe
okomojacob <okomo.code@gmail.com>
2022-11-05 15:22:58 +0300

    Getting started :tada: with components & services, :zap:

---
+++ src/app/courses.service.ts
@@ -0,0 +1,8 @@
+export class CoursesService {
+    getCourses(){
+        return  ["Course 1", "Course 2", "Course 3"];
+    }
+    getAuthors(){
+        return  ["Author 1", "Author 2", "Author 3"];
+    }
+}
